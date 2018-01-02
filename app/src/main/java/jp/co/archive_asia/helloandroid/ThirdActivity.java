f38cfac622ea0dc08f585980e1d2ad06f1b958f4
jaeeun <leejaeeun@archive-asia.co.jp>
2018-01-02 11:42:53 +0900

    ダイアログ練習

--- app/src/main/java/jp/co/archive_asia/helloandroid/ThirdActivity.java
+++ app/src/main/java/jp/co/archive_asia/helloandroid/ThirdActivity.java
@@ -1,11 +1,16 @@
 package jp.co.archive_asia.helloandroid;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by jaeeun on 2018/01/01.
@@ -14,6 +19,16 @@ import android.widget.Button;
 public class ThirdActivity extends AppCompatActivity {
 
     Button backSecondButton;
+
+    //Dialog controls
+    TextView selectTextView;
+    Button defaultDlgButton;
+
+    TextView nameTextView;
+    TextView mailTextView;
+    Button customDlgButton;
+
+    View dialogView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,5 +45,92 @@ public class ThirdActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        selectTextView = findViewById(R.id.selectTextView);
+
+        defaultDlgButton = findViewById(R.id.dlgButton);
+        defaultDlgButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dlg = new AlertDialog.Builder(ThirdActivity.this);
+                dlg.setTitle("タイトルはです");
+                dlg.setIcon(R.mipmap.ic_launcher);
+
+                final String[] girlGroups = new String[] {"TWICE","KARA","少女時代"};
+
+//                //list
+//                dlg.setItems(girlGroups, new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        selectTextView.setText(girlGroups[which]);
+//                    }
+//                });
+
+//                //radio
+//                dlg.setSingleChoiceItems(girlGroups, 0, new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        selectTextView.setText(girlGroups[which]);
+//                    }
+//                });
+
+                //checkbox
+                final boolean[] checks = new boolean[] {true, false, false};
+                dlg.setMultiChoiceItems(girlGroups, checks, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                        selectTextView.setText(girlGroups[which] + String.valueOf(isChecked));
+                    }
+                });
+
+                dlg.setPositiveButton("閉じる", null);
+
+
+//
+//                //defult
+//                dlg.setMessage("内容をここに書きます。");
+//                dlg.setPositiveButton("確認", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        Toast.makeText(ThirdActivity.this, "確認ぼたんを押したよ。", Toast.LENGTH_LONG)
+//                        .show();
+//                    }
+//                });
+                dlg.show();
+            }
+        });
+
+        nameTextView = findViewById(R.id.nameTextView);
+        mailTextView = findViewById(R.id.mailTextView);
+        customDlgButton = findViewById(R.id.customDlgButton);
+
+
+        customDlgButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dialogView = View.inflate(ThirdActivity.this, R.layout.dialog, null);
+                AlertDialog.Builder dlg = new AlertDialog.Builder(ThirdActivity.this);
+
+                dlg.setTitle("ユーザー情報入力する");
+                dlg.setIcon(R.drawable.ic_launcher_foreground);
+                dlg.setView(dialogView);
+                dlg.setPositiveButton("確認", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        EditText dlgNameEditText = dialogView.findViewById(R.id.nameEditText);
+                        EditText dlgMailEditText = dialogView.findViewById(R.id.mailEditText);
+
+                        nameTextView.setText(dlgNameEditText.getText().toString());
+                        mailTextView.setText(dlgMailEditText.getText().toString());
+                    }
+                });
+                dlg.setNegativeButton("キャンセル", null);
+
+                dlg.show();
+
+            }
+        });
+
     }
 }
