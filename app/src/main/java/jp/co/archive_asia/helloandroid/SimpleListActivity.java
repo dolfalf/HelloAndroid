ede5e4d997a9b9d23bda1a37ad8ce69e8f9b3f09
jaeeun <leejaeeun@archive-asia.co.jp>
2018-01-02 16:07:34 +0900

    同的テーブルビュー練習

--- app/src/main/java/jp/co/archive_asia/helloandroid/SimpleListActivity.java
+++ app/src/main/java/jp/co/archive_asia/helloandroid/SimpleListActivity.java
@@ -6,8 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 /**
  * Created by jaeeun on 2018/01/02.
@@ -16,6 +20,8 @@ import android.widget.Toast;
 public class SimpleListActivity extends AppCompatActivity {
 
     private ListView listView;
+    private Button addButton;
+    private EditText addEditText;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,6 +31,8 @@ public class SimpleListActivity extends AppCompatActivity {
 
         setTitle("ListView練習");
 
+        listView = findViewById(R.id.listView1);
+/*
         final String[] languages = {
                 "Objective-C",
                 "Java",
@@ -41,11 +49,19 @@ public class SimpleListActivity extends AppCompatActivity {
                 "HTML/CSS",
                 "Aduino"};
 
-        listView = findViewById(R.id.listView1);
 
+
+        //default
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+//                android.R.layout.simple_list_item_1,
+//                languages);
+
+        //checkbox
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1,
+                android.R.layout.simple_list_item_multiple_choice,
                 languages);
+        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
 
         listView.setAdapter(adapter);
 
@@ -56,5 +72,35 @@ public class SimpleListActivity extends AppCompatActivity {
                 .show();
             }
         });
+*/
+
+        final ArrayList<String> items = new ArrayList<String>();
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,items);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                items.remove(position);
+                adapter.notifyDataSetChanged();
+                return false;
+            }
+        });
+
+        addButton = findViewById(R.id.addButton);
+        addEditText = findViewById(R.id.addItemEditText);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                items.add(addEditText.getText().toString());
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
+
+
 }
